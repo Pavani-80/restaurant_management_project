587dc1b9bd55914a20837e8e629d35f1472e0d32
Pavani Chepalle <chepallepavani777@gmail.com>
2025-08-08 12:04:46 +0530

    23391

--- home/views.py
+++ home/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request, 'home/index.html')
+def custom_404(request, exception):
+    return render(request, '404.html', status=404)
 
-# Create your views here.
