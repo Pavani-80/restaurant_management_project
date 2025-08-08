2aa7c799aff56220d2e51047796739d98b732c39
Pavani Chepalle <chepallepavani777@gmail.com>
2025-08-08 13:24:29 +0530

    23393

--- home/views.py
+++ home/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 def custom_404(request, exception):
     return render(request, '404.html', status=404)
 
+def home_view(request):
+    context = { 
+        'restaurant_name': settings.RESTAURANT_NAME
+    }
+    return render(request, 'home.html', context)
