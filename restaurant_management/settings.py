96b91c3930b93e91ea844eafbdf23cce055ed250
Pavani Chepalle <chepallepavani777@gmail.com>
2025-08-10 14:12:09 +0530

    26897

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 RESTAURANT_NAME = "My Awesome restaurant"
 RESTAURANT_PHONE = "+91 98765 43210"
 
