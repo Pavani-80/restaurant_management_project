c2ff046aa00fa6811ddfe31dcfbe4c0053c011e7
Pavani Chepalle <chepallepavani777@gmail.com>
2025-08-16 22:09:20 +0530

    32666

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Feedback, UserProfile, Menu, Order
 
-# Register your models here.
+@admin.registe(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ("id", "comment", "created_at")
+    ordering = ("created_at",)
+
+@admin.register(UserProfile)
+class UserProifleAdmin(admin.ModelAdmin):
+    list_display = ("user", "phonr_number")
+
+@admin.register(Menu):
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ("name", "price")
+    search_fields = ("name",)
+
+@admin.register(admin.ModelAdmin):
+list_display = ("id", "customer", "total_amount", "status", "created_at")
+list_filter = ("status", "created_at")
+search_fields = ("customer__username",)
+
+
