0e95d0718d99acc4c6441cab4478f846e2c877fb
Pavani Chepalle <chepallepavani777@gmail.com>
2025-08-16 22:01:57 +0530

    32665

--- home/models.py
+++ home/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-class Feedback(models.Model):
+from django.contrib.auth.models import User
 
+class Feedback(models.Model):
     comment = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -14,7 +15,30 @@ class UserProfile(models.Model):
     def __str__(self):
         return f"{self.user.username} - {self.phone_number or 'No phone'}"
 
+class Menu(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
 
+    def __str__(self):
+        return self.name:
+
+class Order(models.Model):
+    STATUS_CHOICES = [
+        ('pending', 'pending'),
+        ('confirmed', 'confirmed'),
+        ('completed', 'completed'),
+        ('cancelled', 'cancelled'),
+    ]
+
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    items = models.ManyToManyField(Menu)
+    total_Amount = models.DecimalField(max_digits=10, decimal+places=2)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES), defult='pending')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order {self.id} - {self.customer.username} - {self.status}"
 
 
 
