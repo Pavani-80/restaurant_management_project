ecc4acaa63918e71e2203124ef339dd36a56dc2c
Pavani Chepalle <chepallepavani777@gmail.com>
2025-08-16 21:08:23 +0530

    32663

--- home/models.py
+++ home/models.py
@@ -5,7 +5,15 @@ class Feedback(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Feedback {self.id} - {self.created_at.strftime('%y-%m-%d)}"
+        return f"Feedback {self.id} - {self.created_at.strftime('%y-%m-%d')}"
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    Phone_number = models.CharField(max_length=15, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.phone_number or 'No phone'}"
+
 
 
 
