49fec04075fe76ad1821d218e580dc34b8a697e1
Pavani Chepalle <chepallepavani777@gmail.com>
2025-08-16 12:26:02 +0530

    32660

--- home/models.py
+++ home/models.py
@@ -2,9 +2,10 @@ from django.db import models
 class Feedback(models.Model):
 
     comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.comment[:50]
+        return f"Feedback {self.id} - {self.created_at.strftime('%y-%m-%d)}"
 
 
 
