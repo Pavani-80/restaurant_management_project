00ab2c4f94123908b993fdc4d4b24c5328be704e
Pavani Chepalle <chepallepavani777@gmail.com>
2025-08-21 20:02:33 +0530

    35722

--- home/models.py
+++ home/models.py
@@ -10,7 +10,7 @@ class Feedback(models.Model):
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    Phone_number = models.CharField(max_length=15, blank=True, null=True)
+    Phone_number = models.CharField(max_length=100)
 
     def __str__(self):
         return f"{self.user.username} - {self.phone_number or 'No phone'}"
