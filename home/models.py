6f3e4dd9948742f159b9bec7018ed9e448a09111
Pavani Chepalle <chepallepavani777@gmail.com>
2025-08-21 20:08:53 +0530

    35721

--- home/models.py
+++ home/models.py
@@ -40,6 +40,14 @@ class Order(models.Model):
     def __str__(self):
         return f"Order {self.id} - {self.customer.username} - {self.status}"
 
+class ContactSubmission(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    submitted_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.email}"
+
 
 
 
