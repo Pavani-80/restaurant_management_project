f00eef50b7c9db72e153479611fc625ce857d048
Pavani Chepalle <chepallepavani777@gmail.com>
2025-08-24 15:10:16 +0530

    Update admin.py

--- home/admin.py
+++ home/admin.py
@@ -1,24 +1,32 @@
 from django.contrib import admin
-from .models import Feedback, UserProfile, Menu, Order
-from .models import MenuItem
+from .models import Feedback, UserProfile, Menu, Order, ContactSubmission
 
-@admin.registe(Feedback)
+
+@admin.register(Feedback)
 class FeedbackAdmin(admin.ModelAdmin):
     list_display = ("id", "comment", "created_at")
     ordering = ("created_at",)
 
+
 @admin.register(UserProfile)
-class UserProifleAdmin(admin.ModelAdmin):
-    list_display = ("user", "phonr_number")
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "phone_number")
 
-@admin.register(Menu):
+
+@admin.register(Menu)
 class MenuAdmin(admin.ModelAdmin):
-    list_display = ("name", "price")
-    search_fields = ("name",)
+    list_display = ("name", "price", "description")
 
-@admin.register(admin.ModelAdmin):
-list_display = ("id", "customer", "total_amount", "status", "created_at")
-list_filter = ("status", "created_at")
-search_fields = ("customer__username",)
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "customer", "total_amount", "status", "created_at")
+    list_filter = ("status", "created_at")
+    search_fields = ("customer__username",)
+
+
+@admin.register(ContactSubmission)
+class ContactSubmissionAdmin(admin.ModelAdmin):
+    list_display = ("name", "email", "submitted_at")
 
 
