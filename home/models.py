84b851caf9e3b6bad7816ff75ddef67e4df986fa
Pavani Chepalle <chepallepavani777@gmail.com>
2025-08-24 15:09:20 +0530

    Update models.py

--- home/models.py
+++ home/models.py
@@ -8,12 +8,14 @@ class Feedback(models.Model):
     def __str__(self):
         return f"Feedback {self.id} - {self.created_at.strftime('%y-%m-%d')}"
 
+
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    Phone_number = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return f"{self.user.username} - {self.phone_number or 'No phone'}"
+
 
 class Menu(models.Model):
     name = models.CharField(max_length=100)
@@ -21,24 +23,26 @@ class Menu(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2)
 
     def __str__(self):
-        return f"{self.name} - {self.Price}";
+        return f"{self.name} - {self.price}"
+
 
 class Order(models.Model):
     STATUS_CHOICES = [
-        ('pending', 'pending'),
-        ('confirmed', 'confirmed'),
-        ('completed', 'completed'),
-        ('cancelled', 'cancelled'),
+        ('pending', 'Pending'),
+        ('confirmed', 'Confirmed'),
+        ('completed', 'Completed'),
+        ('cancelled', 'Cancelled'),
     ]
 
     customer = models.ForeignKey(User, on_delete=models.CASCADE)
     items = models.ManyToManyField(Menu)
-    total_Amount = models.DecimalField(max_digits=10, decimal+places=2)
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES), defult='pending')
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"Order {self.id} - {self.customer.username} - {self.status}"
+
 
 class ContactSubmission(models.Model):
     name = models.CharField(max_length=100)
@@ -47,7 +51,3 @@ class ContactSubmission(models.Model):
 
     def __str__(self):
         return f"{self.name} ({self.email})"
-
-
-
-
