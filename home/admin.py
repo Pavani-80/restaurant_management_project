8b98a339f06f7ab9da80556dfbdcdf401172c7d4
Pavani Chepalle <chepallepavani777@gmail.com>
2025-08-25 11:46:43 +0530

    37262

--- home/admin.py
+++ home/admin.py
@@ -1,22 +1,25 @@
 from django.contrib import admin
-from .models import Feedback, UserProfile, Menu, Order, ContactSubmission
-
+from .models import Feedback, UserProfile, Menu, Order
+from .models import MenuItem
 
 @admin.register(Feedback)
 class FeedbackAdmin(admin.ModelAdmin):
     list_display = ("id", "comment", "created_at")
     ordering = ("created_at",)
 
-
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ("user", "phone_number")
 
-
 @admin.register(Menu)
 class MenuAdmin(admin.ModelAdmin):
-    list_display = ("name", "price", "description")
+    list_display = ("name", "price")
+    search_fields = ("name",)
 
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ("name", "menu", "price")
+    search_fields = ("name")
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
@@ -24,9 +27,8 @@ class OrderAdmin(admin.ModelAdmin):
     list_filter = ("status", "created_at")
     search_fields = ("customer__username",)
 
-
-@admin.register(ContactSubmission)
-class ContactSubmissionAdmin(admin.ModelAdmin):
-    list_display = ("name", "email", "submitted_at")
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ("name", "phone", "email", "address")
 
 
