1f542ac97f0945fed2ecc5729cec609790320c92
Pavani Chepalle <chepallepavani777@gmail.com>
2025-08-25 11:59:49 +0530

    37263

--- home/models.py
+++ home/models.py
@@ -15,11 +15,11 @@ class UserProfile(models.Model):
     def __str__(self):
         return f"{self.user.username} - {self.phone_number or 'No phone'}"
 
-class Menu(models.Model):
+class MenuItem(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=8, decimal_places=2)
-
+    image = models.ImageField(upload_to="menu_images/", blank=True, null=True)
     def __str__(self):
         return f"{self.name} - {self.Price}";
 
