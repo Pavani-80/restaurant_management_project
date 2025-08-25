3608d171fb6db9b9ab0c3c78c928e51de21ccb89
Pavani Chepalle <chepallepavani777@gmail.com>
2025-08-25 11:38:54 +0530

    37262

--- home/models.py
+++ home/models.py
@@ -8,14 +8,12 @@ class Feedback(models.Model):
     def __str__(self):
         return f"Feedback {self.id} - {self.created_at.strftime('%y-%m-%d')}"
 
-
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone_number = models.CharField(max_length=100, blank=True, null=True)
+    Phone_number = models.CharField(max_length=100)
 
     def __str__(self):
         return f"{self.user.username} - {self.phone_number or 'No phone'}"
-
 
 class Menu(models.Model):
     name = models.CharField(max_length=100)
@@ -23,26 +21,24 @@ class Menu(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2)
 
     def __str__(self):
-        return f"{self.name} - {self.price}"
-
+        return f"{self.name} - {self.Price}";
 
 class Order(models.Model):
     STATUS_CHOICES = [
-        ('pending', 'Pending'),
-        ('confirmed', 'Confirmed'),
-        ('completed', 'Completed'),
-        ('cancelled', 'Cancelled'),
+        ('pending', 'pending'),
+        ('confirmed', 'confirmed'),
+        ('completed', 'completed'),
+        ('cancelled', 'cancelled'),
     ]
 
     customer = models.ForeignKey(User, on_delete=models.CASCADE)
     items = models.ManyToManyField(Menu)
-    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+    total_Amount = models.DecimalField(max_digits=10, decimal+places=2)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES), defult='pending')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"Order {self.id} - {self.customer.username} - {self.status}"
-
 
 class ContactSubmission(models.Model):
     name = models.CharField(max_length=100)
@@ -51,3 +47,16 @@ class ContactSubmission(models.Model):
 
     def __str__(self):
         return f"{self.name} ({self.email})"
+
+class Restauarnt(models.Model):
+    name = models.CharField(max_length=200, default="hungry Vibes")
+    phone = models.CharField(max_length=20, default="+91 98765 43210")
+    email = models.CharField(default="hungryvibes@gmail.com")
+    address = models.TextField(default="123 Food street, Banglore, India")
+
+    def__str__(self):
+        return self.name
+
+
+
+
