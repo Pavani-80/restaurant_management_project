1cf3eff7de6987ea28e29c2d5021f928f1b8cd9a
Pavani Chepalle <chepallepavani777@gmail.com>
2025-08-31 11:03:27 +0530

    40201

--- home/forms.py
+++ home/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import contact
 
 class ContactForm(forms.ModelForm):
-    class Meta:
-        model = contact
-        fields = ["name", "email"]
+    name = forms.CharField(max_length=100, required=True)
+    email = forms.EmailFIeld(required=True)
+    message = forms.CharField(widget=forms.Textarea, required=True)
+   
